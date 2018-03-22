5809c03a611cfda7144493a5b303ba3d6e5fbf17
kyob1010 <kyob1010@gmail.com>
2018-03-22 21:10:21 +0800

    some change ...?

--- index.js
+++ index.js
@@ -4,108 +4,131 @@
 
 const fs = require('fs');
 const util = require('util');
+const path = require('path');
+
+////////////////////////////////////////////////////////////////////////
+//                             Constants                              //
+////////////////////////////////////////////////////////////////////////
+
+const QUOTE_FILENAME = 'quote.json';
 
 /////////////////////////////
 //           Quote         //
 /////////////////////////////
 
-// 載入指令表
-const QUOTE_FILENAME = 'quote.json';
-let quotedatas = require("./" + QUOTE_FILENAME);
-
 function getUsage(command) {
-  return "你輸入的指令有錯誤，不過作者還沒寫文件 (诶?";
+  switch (command) {
+  case "quote":
+    return "!quote <user>";
+    break;
+  case "quote <user>":
+    return "!quote <user> <add|delete|list|random|quote id>"
+      break;
+  default:
+    return "你輸入的指令有錯誤，不過作者還沒寫文件 (诶?";
+  }
 }
 
 function userTagToUserId(userTag) {
 	return userTag.match(/^<@!?(\d+)>$/, "")[0];
 }
 
-function isUserTag(userTag) {
-	if (/^<@!?\d+>$/.test(userTag)) return true;
-	else return false;
-}
-
-function usernameToUserId(username, members) {
-  for (let collection_snowflake_guildmember of members) {
-    let id = collection_snowflake_guildmember[0];
-    let guildmember = collection_snowflake_guildmember[1];
-    if (username === guildmember.displayName) return id;
-  }
-  return null;
-}
-
-function getUserDisplayNameWithUserId(userId, members) {
-  for (let collection_snowflake_guildmember of members) {
-    let id = collection_snowflake_guildmember[0];
-    let guildmember = collection_snowflake_guildmember[1];
-    if (id === userId) return guildmember.displayName;
-  }
-  return null;
-}
-
 function quoteFormat(quote, speaker, timestamp) {
   let d = new Date(timestamp);
-  return `"${quote}" - ${speaker}(${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()})`;
+  return `「${quote}」 - ${speaker}(${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()})`;
 }
 
 class Quote {
   constructor(register) {
-    this.register = register;
+    // load quotes
+    try {
+      this.quotedatas = require("./" + QUOTE_FILENAME);
+    } catch (e) {
+      throw new Error(`quote data load fail`);
+    }
+    //
     register.registEvent('exit', this.exitHandle.bind(this));
     register.registCommand('!quote', this.quoteHandle.bind(this));
   }
-  
-  quoteHandle(message, ...args) {
-    // 防呆
-    if (args[2] === "") args.splice(2, 1);
+
+  processUserArgument(message, user) {
+    let id, name;
+    // test if user is a tag
+    if (/^<@!?\d+>$/.test(user)) {
+      id = user.match(/^<@!?(\d+)>$/)[1];
+      name = message.guild.members.find(guildmember => guildmember.id == id).nickname || message.client.users.find(u => u.id == id).username;
+    } else {
+      // assume it's username
+      let u = message.guild.members.find(guildmember => guildmember.nickname == user) || message.client.users.find(u => u.username == user);
+      if (u != null) id = u.id;
+      name = user;
+    }
     //
+    if (!id || !name) throw new Error("username process Error");
+    //
+    return {id, name};
+  }
+
+  quoteAddHandle(message, userId, quote) {
+    this.quotedatas[userId].content.push({
+      content: quote, 
+      timestamp: Date.now(), 
+      raw: false, 
+    });
+    //
+    message.reply(`引言「${quote}」添加成功`);
+  }
+
+  quoteHandle(message, ...args) {
+    // arguments alias
+    const quotedatas = this.quotedatas;
     let field_user = args[1];
     let field_method = args[2];
     let field_quote = args.slice(3).join(' ');
     let field_quoteId;
+
+    // 
     let quoteId;
-    let userId;
-    let userDisplayname = null;
+    let userId, userDisplayName;
     //
-    if (isUserTag(field_user)) {
-      userId = field_user.match(/^<@!?(\d+)>$/)[1];
-      userDisplayname = getUserDisplayNameWithUserId(userId, message.guild.members);
-    }
-    else {
-      // assume it's username
-      userId = usernameToUserId(field_user, message.guild.members);
-      userDisplayname = field_user;
-    }
-    if (userId === null || userDisplayname === null) {
-      message.reply(`無法找到使用者 ${field_user}`);
+    if (field_user == undefined) {
+      message.reply(getUsage('quote'));
       return;
     }
+    try {
+      //
+      let u = this.processUserArgument(message, field_user);
+      userId = u.id;
+      userDisplayName = u.name;
+    } catch (e) {
+      // can't find user, use raw name instead
+      userId = field_user;
+      userDisplayName = field_user;
+    }
+
     // initialized
     if (!(userId in quotedatas)) quotedatas[userId] = { content: [] }
     // quote
     let quotedata = quotedatas[userId];
     let quotes = quotedatas[userId].content;
-    
+    //
     switch (field_method) {
       case 'a': // same as add
       case 'add':
-        quotedatas[userId].content.push({
-          content: field_quote, 
-          timestamp: Date.now(), 
-          raw: false, 
-        });
-        //
-        message.reply(`引言「${field_quote}」添加成功`);
+        this.quoteAddHandle(message, userId, field_quote);
         break;
       case 'd': // same as delete
       case 'delete':
-        field_quoteId = args[3];
         if (field_quoteId === undefined) {
-          message.reply(getUsage("!quote <user> delete"));
+          message.reply(getUsage("quote <user> delete"));
           return;
         }
-        quoteId = Number(field_quoteId);
+        try {
+          quoteId = Number(field_quoteId);
+        } catch(e) {
+          message.reply(`${field_quoteId} 不是一個有效的數字`);
+          return;
+        }
         if (isNaN(quoteId)) {
           message.reply(`您輸入的 id (${id}) 有誤`);
           return;
@@ -121,23 +144,20 @@ class Quote {
           return;
         }
         //
-        message.reply(`刪除引言 \`${quoteFormat(quotes[quoteId].content, userDisplayname, quotes[quoteId].timestamp)}\`成功`);
+        message.reply(`刪除引言 \`${quoteFormat(quotes[quoteId].content, userDisplayName, quotes[quoteId].timestamp)}\`成功`);
         quotes.splice(quoteId, 1);
-        break;
-      case 'e': // same as edit
-      case 'edit':
         break;
       case 'l': // same as list
       case 'list':
         if (quotes.length === 0) {
-          message.channel.send(`${userDisplayname} 沒有任何引言`);
+          message.channel.send(`${userDisplayName} 沒有任何引言`);
           return;
         }
         let response = "";
         for (let id = 0; id < quotes.length; ++id) {
           let quote = quotes[id];
           let displayId = id + 1;
-          response += displayId + ": " + quoteFormat(quote.content, userDisplayname, quote.timestamp) + "\n";
+          response += displayId + ": " + quoteFormat(quote.content, userDisplayName, quote.timestamp) + "\n";
         }
         message.channel.send(response);
         return;
@@ -145,20 +165,20 @@ class Quote {
       case 'r': // Same as random
       case 'random':
         if (quotes.length === 0) {
-          message.channel.send(`${userDisplayname} 沒有任何引言`);
+          message.channel.send(`${userDisplayName} 沒有任何引言`);
           return;
         }
         quoteId = Math.floor(Math.random() * quotes.length);
         let quote = quotes[quoteId];
-        message.channel.send(quoteFormat(quote.content, userDisplayname, quote.timestamp));
-          return;
+        message.channel.send(quoteFormat(quote.content, userDisplayName, quote.timestamp));
+        return;
         break;
       default:
         field_quoteId = args[2];
         quoteId = Number(field_quoteId);
         //
         if (field_quoteId === undefined) {
-          message.reply(getUsage("!quote <user>"));
+          message.reply(getUsage("quote <user>"));
           return;
         }
         //
@@ -170,7 +190,7 @@ class Quote {
         //
         if (Number.isInteger(quoteId)) {
           if (quotes.length === 0) {
-            message.channel.send(`${userDisplayname} 沒有任何引言`);
+            message.channel.send(`${userDisplayName} 沒有任何引言`);
             return;
           }
           --quoteId; // to programming id
@@ -179,7 +199,7 @@ class Quote {
             return;
           }
           let quote = quotes[quoteId];
-          message.channel.send(quoteFormat(quote.content, userDisplayname, quote.timestamp));
+          message.channel.send(quoteFormat(quote.content, userDisplayName, quote.timestamp));
           return;
         } else {
           message.reply('該 method 目前並不支援');
@@ -189,11 +209,7 @@ class Quote {
   }
   
   saveQuoteSync() {
-    try {
-      fs.writeFileSync("./plugins/" + QUOTE_FILENAME, JSON.stringify(quotedatas));
-    } catch (e) {
-      console.error("捕捉到錯誤: " + e);
-    }
+    fs.writeFileSync(path.join(__dirname, QUOTE_FILENAME), JSON.stringify(this.quotedatas));
   }
   
   exitHandle() {
